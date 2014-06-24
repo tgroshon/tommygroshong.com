bd1f4ee2873493c6f84dbb430717047306071ab9
Tommy Groshong <tgroshong@instructure.com>
2014-06-24 08:55:47 -0600

    Made the website into a real project.
     - Broccoli.js for asset pipeline.
     - src/ and dist/ directories.
     - Build process with "npm run build".
     - Updated .gitignore for dist/ and tmp/ dirs.

---
+++ Brocfile.js
@@ -0,0 +1,20 @@
+
+var pickFiles = require('broccoli-static-compiler');
+var cleanCSS = require('broccoli-clean-css');
+var gzipFiles = require('broccoli-gzip');
+var htmlmin = require('broccoli-htmlmin');
+
+var tree = cleanCSS('src');
+
+tree = htmlmin(tree);
+
+/*
+tree = gzipFiles(tree, {
+	  extensions: ['js', 'css', 'png', 'jpg']
+});
+*/
+
+module.exports = pickFiles(tree, {
+  srcDir: '/',
+  destDir: '/'
+})
