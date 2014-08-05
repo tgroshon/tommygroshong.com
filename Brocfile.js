cfea9174865ef47e2b1d3c7d591d51083ac12d97
Tommy Groshong <tgroshong@instructure.com>
2014-08-04 20:10:18 -0600

    Comment out broccoli gzipping

--- Brocfile.js
+++ Brocfile.js
@@ -1,7 +1,7 @@
 
 var pickFiles = require('broccoli-static-compiler');
 var cleanCSS = require('broccoli-clean-css');
-var gzipFiles = require('broccoli-gzip');
+// var gzipFiles = require('broccoli-gzip');
 var htmlmin = require('broccoli-htmlmin');
 
 var tree = cleanCSS('src');
