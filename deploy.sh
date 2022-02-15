b9b0cce8b7282eb6c035bc0c354486820181d36b
Tommy Groshong <tom@groshong.software>
2022-02-14 20:15:48 -0500

    Better deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-aws s3 cp --recursive public s3://tommygroshong.com --cache-control max-age=500 --profile tommy
+rm -rf public;
+hugo;
+aws s3 cp --recursive public s3://tommygroshong.com --cache-control max-age=500 --profile tommy;
